cfb76001232e07549d5340ed9e8d1e8eae6aca47
Lee Rottler <clrottler@gmail.com>
2016-05-21 00:01:35 -0700

    Added code to read active sheet of GAVRT file.

--- src/gavrt_pipeline.py
+++ src/gavrt_pipeline.py
@@ -2,4 +2,23 @@
 # observed objects in the data file.
 
 def rd_gvrtdata(file):
-    pass
+    '''
+       Reads in Excel file with GAVRT raw data
+    '''
+
+    import openpyxl
+
+    wrk_book = openpyxl.load_workbook(file)
+    raw_data = wrk_book.active
+    
+    return raw_data
+
+datafl = "../data/data_107_2016.xlsx"
+rd_table = rd_gvrtdata(datafl)
+
+print rd_table['A1'].value
+
+example_list = [1,2,2,2,2,2,3]
+example_set = set(example_list)
+print example_set
+print len(example_set)
